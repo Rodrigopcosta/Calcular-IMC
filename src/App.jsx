38e0d8637e24b4fa8a771f00981b7ce4101979d1
Rodrigo <Rodrigo_ward@hotmail.com>
2024-07-11 20:04:43 -0300

    reindentação de código realizada

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ function App() {
   const [altura, setAltura] = useState('')
   const [mensagem, setMensagem] = useState('')
 
-  function calcularIMC(){
+  function calcularIMC() {
     const alt = altura / 100
     const imc = peso / (alt * alt)
 
-    if(imc < 18.6){
+    if (imc < 18.6) {
       setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
-    }else if(imc >= 18.6 && imc < 24.9){
+    } else if (imc >= 18.6 && imc < 24.9) {
       setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
-    }else if(imc >= 24.9 && imc < 34.9){
+    } else if (imc >= 24.9 && imc < 34.9) {
       setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
-    }else{
+    } else {
       setMensagem("Cuidade Obesidade! Seu IMC: " + imc.toFixed(2))
     }
   }
